9cca4ecf789e08040aaa44329fd2f6701fd43def
herrBez <mirko.bez@studenti.unipd.it>
2017-12-18 22:46:49 +0100

    Refactoring Refactoring of setup and setup_single using the pylint tool: Removed trailing spaces, using spaces instead of tabs, ...

--- setup.py
+++ setup.py
@@ -1,108 +1,112 @@
-import os
 import sys
 import subprocess
 import time
-import json 
+import json
 from functools import reduce
 
-prefix_port = 303
-prefix_rpc_port = 81
-genesis_block_path = 'genesis_block.json'
-genesis_block_url = 'http://genesisblock.altervista.org/genesis_block.json'
+PREFIX_PORT = 303
+PREFIX_RPC_PORT = 81
+GENESIS_BLOCK_PATH = 'genesis_block.json'
+GENESIS_BLOCK_URL = 'http://genesisblock.altervista.org/genesis_block.json'
 
 def start_nodes(conf):
-	for host in range(0, len(conf["nodes"])):
-		client_number = conf["nodes"][host]["client_number"]
-		if client_number > 0:
-			node_ids_string = ""
-			for i in range(0, client_number):
-				tmp = "%02d " % (i)
-				node_ids_string += tmp
-			cmd = "parallel python setup_single.py ::: " + \
-			node_ids_string + " ::: " + conf["nodes"][host]["login_name"] + "@" + \
-			conf["nodes"][host]["address"] + " ::: " + conf["my_ip"] + " "
-			subprocess.Popen(cmd.split())
+    for host in range(0, len(conf["nodes"])):
+        client_number = conf["nodes"][host]["client_number"]
+        if client_number > 0:
+            node_ids_string = ""
+            for i in range(0, client_number):
+                tmp = "%02d " % (i)
+                node_ids_string += tmp
+            cmd = "parallel python setup_single.py ::: " + \
+            node_ids_string + " ::: " + conf["nodes"][host]["login_name"] + "@" + \
+            conf["nodes"][host]["address"] + " ::: " + conf["my_ip"] + " "
+            subprocess.Popen(cmd.split())
 
 
 def retrieve_enodes(conf):
-	all_enodes = {}
-	l = len(conf["nodes"])
-	print("there are " + str(l) + " hosts")
-	for host in range(0, l):
-		client_number = conf["nodes"][host]["client_number"]
-		if client_number > 0:
-			enodes = [0] * client_number
-			for i in range(0, client_number):
-				rpc_port = "%d%02d " % (prefix_rpc_port, i)
-				cmd = "geth  attach http://" + conf["nodes"][host]["address"] + ":" + rpc_port + " --exec admin.nodeInfo.enode"
-				enodes[i] = subprocess.check_output(cmd.split( ))
-				enodes[i] = enodes[i].decode('ascii').split()[0].replace("[::]", conf["nodes"][host]["address"])
-				# ~ print(enodes[i])
-			all_enodes[str(host)] = enodes
-			# ~ print(all_enodes)
-	return all_enodes
+    all_enodes = {}
+    host_number = len(conf["nodes"])
+    print("there are " + str(host_number) + " hosts")
+    for host in range(0, host_number):
+        client_number = conf["nodes"][host]["client_number"]
+        if client_number > 0:
+            enodes = [0] * client_number
+            for i in range(0, client_number):
+                rpc_port = "%d%02d " % (PREFIX_RPC_PORT, i)
+                cmd = "geth  attach http://" + \
+                conf["nodes"][host]["address"] + ":" + rpc_port + \
+                " --exec admin.nodeInfo.enode"
+                enodes[i] = subprocess.check_output(cmd.split())
+                enodes[i] = enodes[i].decode('ascii').split()[0].\
+                replace("[::]", conf["nodes"][host]["address"])
+                # ~ print(enodes[i])
+            all_enodes[str(host)] = enodes
+            # ~ print(all_enodes)
+    return all_enodes
 
 def add_peers(conf, enodes):
-	print("Add_peers")
-	l = len(conf["nodes"])
-	for host in range(0, l):
-		client_number = conf["nodes"][host]["client_number"]
-		if client_number > 0 :
-			for other_host in range(0, l):
-				other_client_number = conf["nodes"][other_host]["client_number"]
-				if other_client_number > 0:
-					for i in range(0, client_number):
-						for j in range(0, other_client_number):
-								if not (i == j and host == other_host):
-									rpc_port = "%d%02d " % (prefix_rpc_port, i)
-									cmd = "geth attach http://" + conf["nodes"][host]["address"] + ":" + rpc_port + " --exec admin.addPeer(" + enodes[str(other_host)][j] + ")"
-									subprocess.call(cmd.split())
+    print("Add_peers")
+    host_number = len(conf["nodes"])
+    for host in range(0, host_number):
+        client_number = conf["nodes"][host]["client_number"]
+        if client_number > 0:
+            for other_host in range(0, host_number):
+                other_client_number = conf["nodes"][other_host]["client_number"]
+                if other_client_number > 0:
+                    for i in range(0, client_number):
+                        for j in range(0, other_client_number):
+                                if not (i == j and host == other_host):
+                                    rpc_port = "%d%02d " % (PREFIX_RPC_PORT, i)
+                                    cmd = "geth attach http://" + \
+                                    conf["nodes"][host]["address"] + ":" + rpc_port +\
+                                     " --exec admin.addPeer(" + enodes[str(other_host)][j] + ")"
+                                    subprocess.call(cmd.split())
 
 
-#Debug function that checks that the numbers of peers corresponds to 
+#Debug function that checks that the numbers of peers corresponds to
 #the expected one
 def check_number_of_peers(conf):
-	total_peers = reduce(lambda x, y: x+y["client_number"], conf["nodes"], 0)
-	print(total_peers)
-	l = len(conf["nodes"])
-	for host in range(0, l):
-		client_number = conf["nodes"][host]["client_number"]
-		for i in range(0, client_number):
-			rpc_port = "%d%02d " % (prefix_rpc_port, i)
-			cmd = "geth attach http://" + conf["nodes"][host]["address"] + ":" + rpc_port + " --exec net.peerCount"
-			peers = int(subprocess.check_output(cmd.split()).decode('ascii').split()[0])
-			if peers != (total_peers - 1):
-				print("Node %d: found %d peers. Some peers were lost.." % (i, peers))
-				raise
+    total_peers = reduce(lambda x, y: x+y["client_number"], conf["nodes"], 0)
+    print(total_peers)
+    host_number = len(conf["nodes"])
+    for host in range(0, host_number):
+        client_number = conf["nodes"][host]["client_number"]
+        for i in range(0, client_number):
+            rpc_port = "%d%02d " % (PREFIX_RPC_PORT, i)
+            cmd = "geth attach http://" + conf["nodes"][host]["address"]\
+            + ":" + rpc_port + " --exec net.peerCount"
+            peers = int(subprocess.check_output(cmd.split()).decode('ascii').split()[0])
+            if peers != (total_peers - 1):
+                print("Node %d: found %d peers. Some peers were lost.." % (i, peers))
+                sys.exit()
 
 
 
 
 
 def main(argv):
-	if len(sys.argv) < 2:
-		print("Usage: python " + sys.argv[0] + " <conf-file>")
-	else:
-		
-		conf = json.loads(subprocess.check_output((" cat " + sys.argv[1]).split()).decode('ascii'))
-		start_nodes(conf)
-		
-		# TODO: Wait untils services are up and running. sleep should be 
-		# only a temporary solution
-		sleep_time = 10
-		time.sleep(sleep_time)
-		print(str(sleep_time) + " seconds elapsed")
-		
-		# Fetching the enodes of the 
-		enodes = retrieve_enodes(conf)
-		
-		# Add peers "manually" avoiding discovery
-		add_peers(conf, enodes)
-		
-		check_number_of_peers(conf)
-		
-		print("Setup successful..")
+    if len(argv) < 2:
+        print("Usage: python " + sys.argv[0] + " <conf-file>")
+    else:
+
+        conf = json.loads(subprocess.check_output((" cat " + sys.argv[1]).split()).decode('ascii'))
+        start_nodes(conf)
+
+        # TODO: Wait untils services are up and running. sleep should be
+        # only a temporary solution
+        sleep_time = 10
+        time.sleep(sleep_time)
+        print(str(sleep_time) + " seconds elapsed")
+
+        # Fetching the enodes of the
+        enodes = retrieve_enodes(conf)
+
+        # Add peers "manually" avoiding discovery
+        add_peers(conf, enodes)
+
+        check_number_of_peers(conf)
+
+        print("Setup successful..")
 
 if __name__ == "__main__":
     main(sys.argv)
-
--- setup_single.py
+++ setup_single.py
@@ -1,101 +1,104 @@
-import os
 import subprocess
-import time
 import sys
 import errno
 import json
 
 
-prefix_port = 303
-prefix_rpc_port = 81
-genesis_block_url = 'http://genesisblock.altervista.org/genesis_block.json'
+PREFIX_PORT = 303
+PREFIX_RPC_PORT = 81
+GENESIS_BLOCK_URL = 'http://genesisblock.altervista.org/genesis_block.json'
 
 def create_dirs(remote_string_prefix, directory):
-		print("Create dir: %s" % (directory))
-		subprocess.call((remote_string_prefix + " mkdir -p " + directory).split()) 
+    print("Create dir: %s" % (directory))
+    subprocess.call((remote_string_prefix + " mkdir -p " + directory).split())
 
 def download_genesis_block(remote_string_prefix, genesis_block_path):
-	cmd1 = remote_string_prefix + " wget " + genesis_block_url + " -O " + genesis_block_path
-	print(cmd1)
-	subprocess.call(cmd1.split())
+    cmd = remote_string_prefix + " wget " + GENESIS_BLOCK_URL + " -O " + genesis_block_path
+    print(cmd)
+    subprocess.call(cmd.split())
 
 def initialize_blockchain(remote_string_prefix, data_dir, genesis_block_path):
-	# Command to initialize the the blockchain with the genesis_block
-	cmd2 = remote_string_prefix + ' geth --datadir=' + data_dir + ' init ' + genesis_block_path + " 2>> /dev/null "
-	print(cmd2)
-	subprocess.call(cmd2.split())
+    # Command to initialize the the blockchain with the genesis_block
+    cmd = remote_string_prefix + ' geth --datadir=' + data_dir +\
+    ' init ' + genesis_block_path + " 2>> /dev/null "
+    print(cmd)
+    subprocess.call(cmd.split())
 
 def start_node(remote_string_prefix, data_dir, port, rpc_port, network_id, rpcaddr, rpccorsdomain):
-	# Command to start a geth client that uses our test blockchain
-	cmd3 = remote_string_prefix + 'geth --datadir=' + data_dir + ' --ipcdisable ' + \
-	'--port ' + port + ' --rpcport ' + \
-	rpc_port + ' --rpc --rpcaddr ' + rpcaddr + ' --rpccorsdomain ' +\
-	rpccorsdomain + ' --rpcapi eth,web3,miner,net,admin,personal' +\
-	' --networkid=' + str(network_id) + '  --nodiscover'
-	print (cmd3)
-	subprocess.Popen(cmd3.split())
+    # Command to start a geth client that uses our test blockchain
+    cmd = remote_string_prefix + 'geth --datadir=' + data_dir + ' --ipcdisable ' + \
+    '--port ' + port + ' --rpcport ' + \
+    rpc_port + ' --rpc --rpcaddr ' + rpcaddr + ' --rpccorsdomain ' +\
+    rpccorsdomain + ' --rpcapi eth,web3,miner,net,admin,personal' +\
+    ' --networkid=' + str(network_id) + '  --nodiscover'
+    print(cmd)
+    subprocess.Popen(cmd.split())
 
 
 def main(argv):
-	remote = False
-	login_name = ""
-	server_name = ""
-	rpcaddr = "127.0.0.1"
-	rpccorsdomain = "127.0.0.1"
-	
-	if len(argv) < 2:
-		print("Usage: python %s <node_id> [login_name@server_name] [allowed_rpc_client_ip]" % (argv[0]))
-	else:
-		print(argv[1])
-		i = int(argv[1])
-		if len(argv) > 2:
-			a = argv[2].split("@")
-			login_name = a[0]
-			server_name = a[1]
-			rpcaddr = a[1]
-			remote = True
-			if login_name == "":
-				remote = False
-		if len(argv) > 3:
-			rpccorsdomain = argv[3]
-			print(rpccorsdomain)
-		
-		remote_string_prefix = ""
-		if remote:
-			remote_string_prefix = "ssh " + argv[2] + " "
-		
-		port = "%d%02d" % (prefix_port, i)
-		rpc_port = "%d%02d" % (prefix_rpc_port, i)
-		data_dir = "/tmp/eth/" + str(i)
-		tmp_data_dir = "/tmp/foo/" + str(i)
-		genesis_block_path = 'genesis_block.json'
-		genesis_block_path = tmp_data_dir + "/" + genesis_block_path
-		try:
-			create_dirs(remote_string_prefix, data_dir)
-			create_dirs(remote_string_prefix, tmp_data_dir)
-		except OSError as e:
-			if e.errno != errno.EEXIST:
-				raise  # raises the error again
-		
-		download_genesis_block(remote_string_prefix, genesis_block_path)
-		
-		
-		initialize_blockchain(remote_string_prefix, data_dir, genesis_block_path)
-		
-		genesis_block = json.loads(subprocess.check_output((remote_string_prefix + " cat " + genesis_block_path).split()).decode('ascii'))
-		network_id=genesis_block["config"]["chainId"]
-		
-		start_node(remote_string_prefix,data_dir, port, rpc_port, network_id, rpcaddr, rpccorsdomain)
-		
-		# TODO: Find a portable way to open a new terminal, for now we have
-		# to comment/uncomment the following lines depending on the used
-		# terminal
-		# Konsole
-		#os.system("konsole --new-tab -e bash -c '" + cmd + "'  ");
-		# Ubuntu
-		# os.system("gnome-terminal --tab -x bash -c '" + cmd + "' --"); 
-	
+    remote = False
+    login_name = ""
+    server_name = ""
+    rpcaddr = "127.0.0.1"
+    rpccorsdomain = "127.0.0.1"
+
+    if len(argv) < 2:
+        print("Usage: python %s <node_id> [login_name@server_name]" +\
+        "[allowed_rpc_client_ip]" % (argv[0]))
+    else:
+        print(argv[1])
+        i = int(argv[1])
+        if len(argv) > 2:
+            tmp = argv[2].split("@")
+            login_name = tmp[0]
+            server_name = tmp[1]
+            rpcaddr = tmp[1]
+            remote = True
+            if login_name == "":
+                remote = False
+        if len(argv) > 3:
+            rpccorsdomain = argv[3]
+            print(rpccorsdomain)
+
+        remote_string_prefix = ""
+        if remote:
+            remote_string_prefix = "ssh " + argv[2] + " "
+
+        port = "%d%02d" % (PREFIX_PORT, i)
+        rpc_port = "%d%02d" % (PREFIX_RPC_PORT, i)
+        data_dir = "/tmp/eth/" + str(i)
+        tmp_data_dir = "/tmp/foo/" + str(i)
+        genesis_block_path = 'genesis_block.json'
+        genesis_block_path = tmp_data_dir + "/" + genesis_block_path
+        try:
+            create_dirs(remote_string_prefix, data_dir)
+            create_dirs(remote_string_prefix, tmp_data_dir)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise  # raises the error again
+
+        download_genesis_block(remote_string_prefix, genesis_block_path)
+
+
+        initialize_blockchain(remote_string_prefix, data_dir, genesis_block_path)
+
+        genesis_block = json.loads(subprocess.check_output(\
+        (remote_string_prefix + " cat " + genesis_block_path).\
+        split()).decode('ascii'))
+
+        network_id = genesis_block["config"]["chainId"]
+
+        start_node(remote_string_prefix, data_dir, port, rpc_port,\
+        network_id, rpcaddr, rpccorsdomain)
+
+        # TODO: Find a portable way to open a new terminal, for now we have
+        # to comment/uncomment the following lines depending on the used
+        # terminal
+        # Konsole
+        #os.system("konsole --new-tab -e bash -c '" + cmd + "'  ");
+        # Ubuntu
+        # os.system("gnome-terminal --tab -x bash -c '" + cmd + "' --");
+
 
 if __name__ == "__main__":
     main(sys.argv)
-
