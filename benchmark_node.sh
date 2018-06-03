b5765ad6009fd6d81b9c77e2011a2dfa7d1611be
herrBez <bez.mirko@gmail.com>
2018-06-03 12:14:03 +0200

    Add benchmark node script

---
+++ benchmark_node.sh
@@ -0,0 +1,131 @@
+
+
+start_benchmark() {
+    DATADIR=$1
+    NETWORKID=$2
+    PORT=$3
+    RPCPORT=$4
+    RPCADDR=$5
+    BOOTNODES=$6
+    
+    ETHASH_DIR="$HOME/ethash"
+    ETHASH_CACHE_DIR="$ETHASH_DIR/cache"
+    ETHASH_DAG_DIR="$ETHASH_DIR/dag"
+    
+    
+    KEYSTORE="keystore"
+    RPCCORSDOMAIN="*"
+    RPCAPI="eth,web3,miner,net,admin,personal,debug"
+    
+
+    JS_SCRIPT_PATH=$7
+    OUTPUT_FILE=$8
+    ROLE=$9
+    
+    extra_option = 
+    
+    if [[ $ROLE = "miner" ]]; then
+        extra_option="--minerthreads 1"
+    else
+        extra_option="--js $JS_SCRIPT_PATH"
+    fi
+    
+    
+    timeout -S SIGINT 500s \ 
+        nohup geth \
+        --datadir "$DATADIR" \
+        --keystore "$KEYSTORE" \
+        --ipcdisable \
+        --port "$PORT" \
+        --rpc \
+        --rpcport "$RPCPORT" \
+        --rpcaddr "$RPCADDR" \
+        --rpccorsdomain "$RPCCORSDOMAIN" \
+        --rpcapi "$RPCAPI" \
+        --networkid "$NETWORKID" \
+        --bootnodes "$BOOTNODES" \
+        --metrics \
+        --ethash.cachedir "$ETHASH_CACHE_DIR" \
+        --ethash.dagdir "$ETHASH_DAG_DIR" \
+        --cpuprofile "geth.cpu" \
+        "$extra_option" \
+        >> $OUTPUT_FILE 2>&1 &
+    
+    pid=$!
+    echo "The PID of the program is $pid"
+    
+    chmod +x cpu_mem_info.sh
+    
+    ./cpu_mem_info.sh "$pid" "cpu.csv" &
+    
+    printf "started cpu/mem demon"
+}
+
+
+
+main() { 
+
+    # check arguments
+    if [ $# -lt 4 ]; then
+        printf "Usage: `basename "$0"` <nodes-amount> <first-node-index> <rcpaddr> <bootnodes>\n"
+        exit 1
+    fi
+    node=0
+    ROLE=$1
+    FIRST_NODE_INDEX=$2
+    RCPADDR=$3
+    BOOTNODES=$4
+    
+    
+    BASE_DATADIR="ethtest-datadir-"
+    OUTPUT_DIR="logs"
+    JS_SCRIPTS_DIR="js-scripts"
+
+    # check if output dir already exists
+    check_dir $OUTPUT_DIR
+
+    # check if js scripts dir already exists
+    check_dir $JS_SCRIPTS_DIR
+
+    
+    printf "Configuring node $node ...\n"
+
+    # build datadir and output file string
+    DATADIR=$BASE_DATADIR$node
+    OUTPUT_FILE="$OUTPUT_DIR/node-$node.out"
+    JS_SCRIPT_PATH="$JS_SCRIPTS_DIR/node-$node.js"
+
+    # remove eventual preexisting directory
+    rm -rf $DATADIR
+
+    # init genesis block
+    init_genesis $DATADIR $OUTPUT_FILE
+
+    # start geth node in background
+    NETWORKID=$(read_chainid)
+    PORT=$((30300 + $node))
+    RPCPORT=$((8545 + $node))
+
+    printf "conf = {};
+        conf.accountIndex = $(($FIRST_NODE_INDEX + $node));
+        conf.txDelay = 1000;
+        " | cat - sendTransactions.js > $JS_SCRIPT_PATH
+
+    start_benchmark \
+        "$DATADIR" \
+        "$NETWORKID" \
+        "$PORT" \
+        "$RPCPORT" \
+        "$RCPADDR" \
+        "$BOOTNODES" \
+        "$JS_SCRIPT_PATH" \
+        "$OUTPUT_FILE" \
+        "$ROLE"
+    
+    printf "Node ($RCPADDR:$RPCPORT) started. Output in $OUTPUT_FILE\n"
+}
+
+
+main $ARGS
+
+
