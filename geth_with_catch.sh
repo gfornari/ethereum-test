9f6e0886798fc5a6bc67ec2d0c6c592757d10e12
mbez <mirko.bez@studenti.unipd.it>
2018-06-05 17:24:12 +0200

    Add script to run geth that also catches signals

---
+++ geth_with_catch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Constants used along the program
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+
+catch() {
+    geth attach http://$1:$2 --exec "debug.metrics(false)"
+    pkill geth
+
+    # Do other useful stuffs, e.g. upload stats to central server and so on
+    exit
+}
+
+
+
+
+
+main() {
+
+    trap "catch $4 $5" SIGINT
+
+    geth \
+        --datadir "$1" \
+        --keystore "$2" \
+        --ipcdisable \
+        --port "$3" \
+        --rpc \
+        --rpcport "$4" \
+        --rpcaddr "$5" \
+        --rpccorsdomain "$6" \
+        --rpcapi "$7" \
+        --networkid "$8" \
+        --bootnodes "$9" \
+        --metrics \
+        --ethash.cachedir "${10}" \
+        --ethash.dagdir "${11}" \
+        --cpuprofile "${12}" \
+        ${13} \
+        >> $OUTPUT_FILE 2>&1 &
+
+}
+
+main $ARGS
