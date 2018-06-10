208a020cd47c91bb5e893f8325cbbdbfe3b39790
herrBez <bez.mirko@gmail.com>
2018-06-10 09:32:19 +0200

    Remove useless variables in benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -6,20 +6,9 @@ readonly PROGNAME=$(basename $0)
 readonly PROGDIR=$(readlink -m $(dirname $0))
 readonly ARGS="$@"
 
-readonly BOOTNODE_PORT=29999
-readonly IP_ADDRESS=`ip route get 8.8.8.8 | awk 'NR==1 {print $NF}'`
-readonly NODES_SETUP_SCRIPT="./nodes_setup.sh"
-readonly BENCHMARK_SCRIPT="./benchmark_node.sh"
-
-readonly GIT_REPOSITORY="https://github.com/gfornari/ethereum-test"
-readonly REPO_OUTPUT_DIR="./ethereum-test"
-# https://stackoverflow.com/questions/1593051/how-to-programmatically-determine-the-current-checked-out-git-branch
-readonly BRANCH_NAME=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
-
-
 main() {
     if [[ "$#" -lt "1" ]]; then
-        printf "Usage %s <conf-file> \n" $0
+        printf "Usage %s <conf-file> \n" $PROGNAME
         exit 1;
     fi
 
