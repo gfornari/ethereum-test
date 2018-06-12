c7b82d7193f9da7b836eb83d64ddbef5e0b46ae3
herrBez <bez.mirko@gmail.com>
2018-06-12 22:27:07 +0200

    Remove tmp files to avoid repetition of results

--- geth_with_catch.sh
+++ geth_with_catch.sh
@@ -41,8 +41,11 @@ catch() {
     done 
 
     # Put each entry of the javascript list in a separate line
-    cat /tmp/final_difficulty-$ID.txt | tr "[]" " " | tr " " "\n" >> test/final_difficulty-$ID-$RUN.txt
-    cat /tmp/final_timestamps-$ID.txt | tr "[]" " " | tr " " "\n" >> test/final_timestamps-$ID-$RUN.txt
+    cat /tmp/final_difficulty-$ID.txt | tr "[]" " " | tr " " "\n" > test/final_difficulty-$ID-$RUN.txt
+    cat /tmp/final_timestamps-$ID.txt | tr "[]" " " | tr " " "\n" > test/final_timestamps-$ID-$RUN.txt
+
+    rm /tmp/final_timestamps-$ID.txt
+    rm /tmp/final_timestamps-$ID.txt
 
 
     echo $BLOCK_NUMBER >> test/block_number-$ID.txt
