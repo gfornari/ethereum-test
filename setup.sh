2805a9aabe0001bceab27f376f664d5131b2e110
herrBez <bez.mirko@gmail.com>
2018-06-15 11:18:22 +0200

    Remove quotes

--- setup.sh
+++ setup.sh
@@ -128,7 +128,7 @@ main() {
     timestamp=$(date +%s)
 
     local readonly LOCAL_REPO_DIR=$(basename $GIT_REPOSITORY)
-    if ! [[ -d \"./$LOCAL_REPO_DIR\" ]]; then
+    if ! [[ -d "./$LOCAL_REPO_DIR" ]]; then
         git clone $GIT_REPOSITORY;
     fi;
     cd ./$LOCAL_REPO_DIR
